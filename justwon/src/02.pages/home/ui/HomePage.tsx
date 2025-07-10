7367772ce76834ba87b13427013942d8d8267114
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-07-10 15:00:48 +0900

    add homepage

---
+++ justwon/src/02.pages/home/ui/HomePage.tsx
@@ -0,0 +1,18 @@
+/**
+ * 랜딩페이지.
+ * - 간단한 인삿말
+ * - Recent Posts
+ * - Hot Posts
+ * - Ads (수익 창출용)
+ */
+
+import { Container } from "./styles";
+
+export function HomePage() {
+  return (
+    <Container>
+      <h1>Welcome to JustWon</h1>
+      <p>Your journey starts here.</p>
+    </Container>
+  );
+}
