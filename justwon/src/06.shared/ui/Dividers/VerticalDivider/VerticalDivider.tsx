389265857e9bd9ddc91900b70cb97ad615ae9ca6
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-07-10 17:49:05 +0900

    add divider

---
+++ justwon/src/06.shared/ui/Dividers/VerticalDivider/VerticalDivider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import styled from "styled-components";
+
+interface Props {
+  bold?: boolean;
+  color?: string;
+}
+
+export const VerticalDivider = styled.div<Props>`
+  align-self: stretch;
+  width: ${({ bold }) => (bold ? "2px" : "1px")};
+  background-color: ${({ color }) => color};
+`;
