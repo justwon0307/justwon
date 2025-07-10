c45cb86f0b4ed5c0a0fa6d51e0f0d28ac302e02a
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-07-10 15:02:55 +0900

    fix configs and add packages

--- justwon/next.config.ts
+++ justwon/next.config.ts
@@ -2,6 +2,29 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              icon: true,
+            },
+          },
+        ],
+        as: "*.js",
+      },
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
