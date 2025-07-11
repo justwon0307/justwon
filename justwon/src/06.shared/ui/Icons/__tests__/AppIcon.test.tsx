6af874fde950d2c7f505fb16554f5dddb8d20a79
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-07-11 11:04:39 +0900

    mock svg file to avoid error

--- justwon/src/06.shared/ui/Icons/__tests__/AppIcon.test.tsx
+++ justwon/src/06.shared/ui/Icons/__tests__/AppIcon.test.tsx
@@ -2,6 +2,8 @@ import { render } from "@testing-library/react";
 
 import { AppIcon } from "@shared/ui/Icons";
 
+jest.mock("../files/project.svg", () => () => null);
+
 describe("AppIcon", () => {
   it("should render default icon", () => {
     render(<AppIcon icon="project" />);
