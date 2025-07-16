1b8311438092d6b44eeb256874090efdf6abac7d
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-07-16 18:04:45 +0900

    Add new icons

--- justwon/src/06.shared/ui/Icons/AppIcon/AppIcon.tsx
+++ justwon/src/06.shared/ui/Icons/AppIcon/AppIcon.tsx
@@ -1,5 +1,7 @@
 import About from "../files/about.svg";
 import Admin from "../files/admin.svg";
+import ArrowLeft from "../files/arrow-left.svg";
+import ArrowRight from "../files/arrow-right.svg";
 import Blog from "../files/blog.svg";
 import Login from "../files/login.svg";
 import Project from "../files/project.svg";
@@ -14,6 +16,8 @@ interface Props {
 const iconMap: Record<string, React.FC<React.SVGProps<SVGSVGElement>>> = {
   about: About,
   admin: Admin,
+  "arrow-left": ArrowLeft,
+  "arrow-right": ArrowRight,
   blog: Blog,
   login: Login,
   project: Project,
