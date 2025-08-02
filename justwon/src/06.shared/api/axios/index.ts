80c8794b74ef46e4d15c5c3aed48855ef3bec90e
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-08-02 14:26:00 +0900

    refactor axios to api/ and add base_url config

--- justwon/src/06.shared/api/axios/index.ts
+++ justwon/src/06.shared/api/axios/index.ts
@@ -1,6 +1,12 @@
 import { auth } from "@clerk/nextjs/server";
 import axios from "axios";
 
+/**
+ * 해당 App에서는 axios를 개인화된 요청이 필요할 때만 사용한다.
+ *   - 예시: Bookmarks, 댓글 목록 등
+ *   - 이외의 경우는 fetch를 사용한다.
+ */
+
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 2000, // 2 seconds
