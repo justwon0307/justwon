a650ab40e07f8b99530c7391a57dc980d832b7af
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-08-07 05:16:11 +0900

    add custom error class

--- justwon/src/06.shared/api/models/index.ts
+++ justwon/src/06.shared/api/models/index.ts
@@ -8,4 +8,13 @@ type SuccessResponseType<T> = {
   status: "SUCCESS";
 };
 
+// 커스텀 API 에러
+
+export class APIError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "APIError";
+  }
+}
+
 export type APIResponseType<T> = SuccessResponseType<T> | ErrorResponseType;
