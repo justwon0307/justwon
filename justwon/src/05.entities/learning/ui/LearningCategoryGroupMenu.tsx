f0612a09cb9faf5e7ef796cba396db7f291d204a
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-08-07 05:25:08 +0900

    wrap group menu as one

---
+++ justwon/src/05.entities/learning/ui/LearningCategoryGroupMenu.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import "client-only";
+import { useRouter } from "next/navigation";
+import styled from "styled-components";
+
+import { useLearning } from "../contexts/useLearning";
+import {
+  LearningCategoryGroupType,
+  LearningCategoryType,
+} from "../models/categories";
+import { ExpandableSidebarItem } from "@shared/ui/Sidebar";
+
+interface Props {
+  group: LearningCategoryGroupType;
+}
+
+/**
+ * 학습 카테고리 그룹 메뉴 컴포넌트
+ *   - Interactive하기 때문에, 클라이언트 컴포넌트
+ */
+
+export function LearningCategoryGroupMenu({ group }: Readonly<Props>) {
+  const { selectedCategoryGroup, selectedCategory } = useLearning();
+
+  const router = useRouter();
+
+  const handleCategoryClick = (category: LearningCategoryType) => {
+    router.push(`/learning/${category.category_group_slug}/${category.slug}/`);
+  };
+
+  return (
+    <ExpandableSidebarItem
+      title={group.name}
+      href={`/learning/${group.slug}/`}
+      isActive={group.id === selectedCategoryGroup?.id}
+      key={group.id}
+    >
+      {group.categories.map((category) => (
+        <CategoryButton
+          key={category.id}
+          onClick={() => handleCategoryClick(category)}
+          className={category.id === selectedCategory?.id ? "active" : ""}
+        >
+          <span>{category.name}</span>
+          <span className="count">{category.num_posts}</span>
+        </CategoryButton>
+      ))}
+    </ExpandableSidebarItem>
+  );
+}
+
+const CategoryButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px 12px;
+  text-decoration: none;
+  color: inherit;
+  border-radius: 8px;
+
+  &.active {
+    color: ${({ theme }) => theme.colors.primary};
+    background-color: ${({ theme }) => theme.colors.gray300};
+
+    .count {
+      background-color: ${({ theme }) => theme.colors.backgroundDefault};
+    }
+  }
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.primary};
+    background-color: ${({ theme }) => theme.colors.gray200};
+
+    .count {
+      background-color: ${({ theme }) => theme.colors.backgroundDefault};
+    }
+  }
+
+  .count {
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    text-align: center;
+    font-size: 0.825rem;
+    line-height: 20px;
+    border-radius: 50%;
+    background-color: ${({ theme }) => theme.colors.gray300};
+  }
+`;
