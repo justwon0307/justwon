fc4763811b38b71887a31ce7a5f6997308296950
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-08-06 17:21:21 +0900

    Add subheader wrapper component

--- justwon/src/03.widgets/layouts/ui/styles.ts
+++ justwon/src/03.widgets/layouts/ui/styles.ts
@@ -11,9 +11,40 @@ export const DefaultLayout = styled.div`
 `;
 
 export const ContentWrapper = styled.div`
+  display: flex;
   flex: 1;
+  flex-direction: column;
 `;
 
 export const Divider = styled(VerticalDivider)`
   background-color: ${({ theme }) => theme.colors.divider};
+`;
+
+export const SubheaderWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 4px 24px 4px 8px;
+  border-bottom: 0.5px solid ${({ theme }) => theme.colors.divider};
+
+  .right {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+
+    > a.write-button {
+      display: flex;
+      align-items: center;
+      padding: 6px 12px;
+      background-color: ${({ theme }) => theme.colors.gray200};
+      border: 1px solid ${({ theme }) => theme.colors.gray300};
+      color: ${({ theme }) => theme.colors.primary};
+      border-radius: 8px;
+      font-size: 0.875rem;
+      font-weight: 500;
+      font-family: "Geist", sans-serif;
+      text-decoration: none;
+    }
+  }
 `;
