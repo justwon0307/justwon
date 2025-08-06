1ffab64261557107f5195114a2f786bdf0122682
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-08-06 18:50:33 +0900

    add slug fields to models

--- justwon/src/05.entities/learning/models/categories.ts
+++ justwon/src/05.entities/learning/models/categories.ts
@@ -2,11 +2,15 @@ export type LearningCategoryGroupType = {
   id: number;
   name: string;
   kor_name: string;
+  slug: string;
   categories: LearningCategoryType[];
 };
 
 export type LearningCategoryType = {
   id: number;
   name: string;
+  slug: string;
   num_posts: number;
+  category_group_id: number;
+  category_group_slug: string;
 };
