a9762775b660bd8c66c72fd18dd032bd0858aa12
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-08-13 23:31:11 +0900

    fix test

--- justwon_server/devlog/tests/test_category_group_apis.py
+++ justwon_server/devlog/tests/test_category_group_apis.py
@@ -7,10 +7,7 @@ class BlogCategoryGroupAPITestCase(APITestCase):
     def test_list(self):
         response = self.client.get("/api/v1/blog/groups/")
 
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(
-            len(response.data), 5
-        )  ## 테스트 데이터에 총 5개의 카테고리 그룹이 있다.
+        self.assertEqual(response.status_code, 405)
 
     def test_retrieve(self):
         response = self.client.get("/api/v1/blog/groups/computer-science/")
