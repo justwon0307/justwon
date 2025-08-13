10223ae79d5b7105fc06efcc1e674fe2bd3b739c
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-08-13 15:49:55 +0900

    add styles to error page

--- justwon/src/02.pages/blog/landing/ui/BlogErrorPage.tsx
+++ justwon/src/02.pages/blog/landing/ui/BlogErrorPage.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import "client-only";
+import styled from "styled-components";
+
 interface Props {
   error: Error & { digest?: string };
   reset: () => void;
@@ -5,9 +10,31 @@ interface Props {
 
 export function BlogErrorPage({ error, reset }: Readonly<Props>) {
   return (
-    <div>
+    <Container>
       <p>{error.message}</p>
       <button onClick={() => reset()}>다시 시도하기</button>
-    </div>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 90vh;
+
+  > p {
+    color: ${({ theme }) => theme.colors.error};
+    font-size: 1.2rem;
+    text-align: center;
+  }
+
+  > button {
+    padding: 0.5rem 1rem;
+    color: ${({ theme }) => theme.colors.onPrimary};
+    background-color: ${({ theme }) => theme.colors.primary};
+    border-radius: 0.25rem;
+  }
+`;
