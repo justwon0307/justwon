c2412d954b079532b22a763f34912bc525f3a128
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-08-19 17:26:41 +0900

    fix and refactor rootlayout

---
+++ justwon/src/01.app/root.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+
+import { StylesProvider } from "./providers";
+import { RootHeader } from "@widgets/headers";
+import { geistMono, geistSans } from "@shared/lib/fonts";
+
+export const metadata: Metadata = {
+  title: "JustWon",
+  description: "",
+};
+
+export async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <StylesProvider>
+          <RootHeader />
+          {children}
+        </StylesProvider>
+      </body>
+    </html>
+  );
+}
