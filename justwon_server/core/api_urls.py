7c461780b021be512106613b4ec696add7e6ad5b
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-08-20 15:01:43 +0900

    route apis

---
+++ justwon_server/core/api_urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from devlog.categories.views import CategoryGroupViewSet, CategoryViewSet
+
+router = DefaultRouter()
+
+router.register("v1/blog/groups", CategoryGroupViewSet, basename="category-groups")
+router.register("v1/blog/categories", CategoryViewSet, basename="categories")
+
+urlpatterns = []
+urlpatterns += router.urls
