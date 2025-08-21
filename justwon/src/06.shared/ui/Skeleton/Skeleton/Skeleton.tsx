f351013bca1a19c8fb580e8c6c83bf51c7449610
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-08-21 12:38:16 +0900

    update skeleton component

--- justwon/src/06.shared/ui/Skeleton/Skeleton/Skeleton.tsx
+++ justwon/src/06.shared/ui/Skeleton/Skeleton/Skeleton.tsx
@@ -4,17 +4,27 @@ import "client-only";
 import styled, { keyframes } from "styled-components";
 
 interface Props {
-  $width?: string | number;
-  $height?: string | number;
-  $borderRadius?: string | number;
+  width?: string | number;
+  height?: string | number;
+  borderRadius?: string | number;
 }
 
-export function Skeleton({ $width, $height, $borderRadius }: Readonly<Props>) {
+export function Skeleton({ width, height, borderRadius }: Readonly<Props>) {
+  if (width === 0 || height === 0) {
+    return null; // Avoid rendering if width or height is zero
+  }
+
+  // Default values for width, height, and borderRadius
+  width = typeof width === "number" ? `${width}px` : width || "100%";
+  height = typeof height === "number" ? `${height}px` : height || "1rem";
+  borderRadius =
+    typeof borderRadius === "number"
+      ? `${borderRadius}px`
+      : borderRadius || "8px";
+
   return (
     <SkeletonWrapper
-      $width={$width}
-      $height={$height}
-      $borderRadius={$borderRadius}
+      style={{ width, height, borderRadius }}
       data-testid="skeleton"
     />
   );
@@ -33,12 +43,4 @@ const SkeletonWrapper = styled.div<Props>`
   background: linear-gradient(90deg, #e0e0e0 25%, #f0f0f0 50%, #e0e0e0 75%);
   background-size: 200% 100%;
   animation: ${shimmer} 1.25s ease-in-out infinite;
-  width: ${({ $width }) =>
-    typeof $width === "number" ? `${$width}px` : $width || "100%"};
-  height: ${({ $height }) =>
-    typeof $height === "number" ? `${$height}px` : $height || "1rem"};
-  border-radius: ${({ $borderRadius }) =>
-    typeof $borderRadius === "number"
-      ? `${$borderRadius}px`
-      : $borderRadius || "4px"};
 `;
