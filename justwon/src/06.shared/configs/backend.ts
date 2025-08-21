2c069035e8dc663089682fe37fc42b0b7f15b929
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-08-21 16:56:00 +0900

    add more config variables

--- justwon/src/06.shared/configs/backend.ts
+++ justwon/src/06.shared/configs/backend.ts
@@ -1,1 +1,24 @@
 export const BACKEND_API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+type ErrorResponseType = {
+  message: string;
+  status: "ERROR";
+};
+
+type SuccessResponseType<T> = {
+  data: T;
+  status: "SUCCESS";
+};
+
+// 커스텀 API 에러
+
+export class BackendAPIError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "BackendAPIError";
+  }
+}
+
+export type BackendAPIResponseType<T> =
+  | SuccessResponseType<T>
+  | ErrorResponseType;
