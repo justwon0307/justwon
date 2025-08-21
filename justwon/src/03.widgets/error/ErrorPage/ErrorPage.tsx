d84623b1523f1f8ec60bb131942d10f1ab61245c
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-08-21 14:02:40 +0900

    add error page

--- justwon/src/03.widgets/error/ErrorPage/ErrorPage.tsx
+++ justwon/src/03.widgets/error/ErrorPage/ErrorPage.tsx
@@ -11,7 +11,9 @@ export function ErrorPage({ error, reset }: Readonly<Props>) {
   return (
     <div>
       <h1>Error</h1>
-      <p>Something went wrong. Please try again later.</p>
+      <p>오류가 발생했습니다.</p>
+      <p>{error.message}</p>
+      <button onClick={reset}>다시 시도</button>
     </div>
   );
 }
