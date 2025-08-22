e34a794d0eb7567d8ed75893db03e524e1c13c45
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-08-22 18:22:07 +0900

    remove unused imports

--- justwon/src/test-utils/renderer.tsx
+++ justwon/src/test-utils/renderer.tsx
@@ -1,32 +1,26 @@
 import { JSX, ReactElement, PropsWithChildren } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+//import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render } from "@testing-library/react";
 
-import { ClerkProvider, StylesProvider } from "@app/providers";
+import { StylesProvider } from "@app/root/providers";
 
-export function createTestQueryClient() {
+/*export function createTestQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: { retry: false },
     },
   });
-}
+}*/
 
 export function renderWithProviders(
-  ui: ReactElement,
-  { client = createTestQueryClient() } = {}
+  ui: ReactElement
+  //{ client = createTestQueryClient() } = {}
 ) {
   function Wrapper({ children }: Readonly<PropsWithChildren>): JSX.Element {
-    return (
-      <QueryClientProvider client={client}>
-        <ClerkProvider>
-          <StylesProvider>{children}</StylesProvider>
-        </ClerkProvider>
-      </QueryClientProvider>
-    );
+    return <StylesProvider>{children}</StylesProvider>;
   }
 
-  return { ...render(ui, { wrapper: Wrapper }), client };
+  return { ...render(ui, { wrapper: Wrapper }) }; //, client };
 }
 
 export async function getElementFromAsyncServerComponent<P>(
