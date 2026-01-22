a32a60dc9c64181fc7fb3265291c9affa1fe4c2a
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-01-22 14:54:35 +0900

    add package vitest config

---
+++ frontend/configs/vitest-config/react.js
@@ -0,0 +1,21 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vitest/config";
+
+export const reactConfig = defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    include: ["tests/**/*.test.{ts,tsx}"],
+    coverage: {
+      include: ["src/**/*.tsx"],
+      exclude: ["tests/*"],
+      provider: "v8",
+      reporter: [["text", { skipFull: true }], "clover", "lcov"],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(process.cwd(), "src"),
+    },
+  },
+});
