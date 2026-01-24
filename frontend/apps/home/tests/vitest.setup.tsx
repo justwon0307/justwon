909aec67099f17c92d8fd0bc6f3894e44ddbf81f
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-01-24 19:25:54 +0900

    add mocks config

---
+++ frontend/apps/home/tests/vitest.setup.tsx
@@ -0,0 +1,41 @@
+import { vi } from "vitest";
+
+vi.mock("next/link", () => ({
+  __esModule: true,
+  default: ({
+    children,
+    href,
+    className,
+  }: {
+    children: React.ReactNode;
+    href: string;
+    className?: string;
+  }) => (
+    <a href={href} className={className}>
+      {children}
+    </a>
+  ),
+}));
+vi.mock("next/navigation", () => ({
+  __esModule: true,
+  default: {},
+  usePathname: vi.fn().mockReturnValue("/"),
+}));
+
+vi.mock("@justwon/assets", () => ({
+  JustWonLogo: () => <div>JustWonLogo</div>,
+  JustWonHorizontalLogo: () => <div>JustWonHorizontalLogo</div>,
+}));
+vi.mock("@justwon/icons", () => ({
+  TabIcon: () => <div>TabIcon</div>,
+  ThemeIcon: () => <div>ThemeIcon</div>,
+}));
+vi.mock("@justwon/theme", async () => {
+  const actual = await vi.importActual("@justwon/theme");
+  return {
+    ...actual,
+    ThemeScript: () => <div>ThemeScript</div>,
+    ThemeProvider: ({ children }: { children: React.ReactNode }) => children,
+    useTheme: vi.fn().mockReturnValue({ theme: "light", setTheme: vi.fn() }),
+  };
+});
--- frontend/apps/home/vitest.config.ts
+++ frontend/apps/home/vitest.config.ts
@@ -1,1 +1,11 @@
-export { nextjsConfig as default } from "@justwon/vitest-config/nextjs";
+import { defineConfig, mergeConfig } from "vitest/config";
+
+import { nextjsConfig } from "@justwon/vitest-config/nextjs";
+
+const mocksConfig = defineConfig({
+  test: {
+    setupFiles: ["./tests/vitest.setup.tsx"],
+  },
+});
+
+export default mergeConfig(nextjsConfig, mocksConfig);
