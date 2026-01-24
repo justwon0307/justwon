b2cab26483831db83bcb958424c6823f55cb672a
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-01-24 19:51:50 +0900

    add custom base 404 page

---
+++ frontend/apps/home/app/not-found.tsx
@@ -0,0 +1,8 @@
+import { Metadata } from "next";
+
+export { NotFound as default } from "@widgets/not-found";
+
+export const metadata: Metadata = {
+  title: "404 | JustWon",
+  description: "죄송합니다. 찾고 있는 페이지가 존재하지 않습니다.",
+};
