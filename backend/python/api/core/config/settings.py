e8caaa23654b5bbd149346356207250af2d17d4f
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-02-02 17:11:08 +0900

    add models to project

--- backend/python/api/core/config/settings.py
+++ backend/python/api/core/config/settings.py
@@ -33,6 +33,12 @@ INSTALLED_APPS = [
   "django.contrib.sessions",
   "django.contrib.messages",
   "django.contrib.staticfiles",
+
+  "apps.blog.categories",
+  "apps.blog.posts",
+  "apps.blog.posttypes",
+  "apps.blog.series",
+  "apps.blog.tags",
 ]
 
 MIDDLEWARE = [
@@ -71,7 +77,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
   "default": {
     "ENGINE": "django.db.backends.sqlite3",
-    "NAME": BASE_DIR / "db.sqlite3",
+    "NAME": BASE_DIR("db.sqlite3"),
   }
 }
 
