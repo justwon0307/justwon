1788fde9cd494d4093444a2748438ce54c07bc1d
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-02-12 16:27:10 +0900

    bootstrap app:

--- frontend/apps/studio/src/main.tsx
+++ frontend/apps/studio/src/main.tsx
@@ -1,9 +1,40 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { AuthProvider } from "@justkits/auth";
+import "@justwon/designs/globals.css";
+
+import { LoadingWidget } from "@widgets/loading";
+import { refreshTokenAPI } from "@features/auth/refresh-token";
+import { api } from "@shared/api/axios";
+import { routeTree } from "./routeTree.gen.ts";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    mutations: {
+      retry: false,
+    },
+  },
+});
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider
+        instance={api}
+        fallback={<LoadingWidget />}
+        tokenRefreshAPICall={refreshTokenAPI}
+      >
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
