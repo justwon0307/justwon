bbd7ad3919656cf52a3746c5385525fc472eaed1
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-02-12 16:24:55 +0900

    add tanstack router, tanstack query, auth and path alias to project

--- frontend/apps/studio/eslint.config.ts
+++ frontend/apps/studio/eslint.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "eslint/config";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 import { config } from "@justwon/eslint-config/vite";
 
 export default defineConfig([
+  ...pluginQuery.configs["flat/recommended"],
   {
     extends: [config],
     languageOptions: {
--- frontend/apps/studio/vite.config.ts
+++ frontend/apps/studio/vite.config.ts
@@ -1,8 +1,28 @@
 import { defineConfig } from "vite";
+import path from "node:path";
 import react from "@vitejs/plugin-react";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [
+    // tanstack plugin이 react plugin보다 먼저와야한다
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+    vanillaExtractPlugin(),
+  ],
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "src/01.app"),
+      "@pages": path.resolve(__dirname, "src/02.pages"),
+      "@widgets": path.resolve(__dirname, "src/03.widgets"),
+      "@features": path.resolve(__dirname, "src/04.features"),
+      "@entities": path.resolve(__dirname, "src/05.entities"),
+      "@shared": path.resolve(__dirname, "src/06.shared"),
+    },
+  },
 });
