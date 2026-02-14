4abf6eb058c7ec35b73322d84e11293c62998b91
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-02-14 14:12:27 +0900

    fix icon namd

--- frontend/packages/design-system/src/icons/components/tab/TabIcon.tsx
+++ frontend/packages/design-system/src/icons/components/tab/TabIcon.tsx
@@ -1,16 +1,18 @@
 import { AboutIcon } from "./components/AboutIcon";
 import { BlogIcon } from "./components/BlogIcon";
-import { NonDevIcon } from "./components/NonDevIcon";
+import { CoffeeIcon } from "./components/CoffeeIcon";
 import { ProjectsIcon } from "./components/ProjectsIcon";
 import { IconProps } from "../types";
 
 interface Props extends IconProps {
-  tab: "about" | "devlog" | "non-dev" | "projects";
+  tab: "about" | "devlog" | "lounge" | "projects";
 }
 
-export function TabIcon({ tab, size = 24, ...rest }: Readonly<Props>) {
-  if (tab === "about") return <AboutIcon size={size} {...rest} />;
-  if (tab === "devlog") return <BlogIcon size={size} {...rest} />;
-  if (tab === "non-dev") return <NonDevIcon size={size} {...rest} />;
-  return <ProjectsIcon size={size} {...rest} />;
+export function TabIcon({ tab, ...rest }: Readonly<Props>) {
+  const iconSize = 20;
+
+  if (tab === "about") return <AboutIcon size={iconSize} {...rest} />;
+  if (tab === "devlog") return <BlogIcon size={iconSize} {...rest} />;
+  if (tab === "lounge") return <CoffeeIcon size={iconSize} {...rest} />;
+  return <ProjectsIcon size={iconSize} {...rest} />;
 }
