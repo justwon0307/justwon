af3b6c3721645f2a3ca2dbcda5b3102e7d248251
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-03-06 00:22:52 +0900

    add studio logo components

--- frontend/packages/design-system/src/brand/index.ts
+++ frontend/packages/design-system/src/brand/index.ts
@@ -1,2 +1,5 @@
 export { JustwonHorizontalLogo } from "./components/JustwonHorizontalLogo";
 export { JustwonLogo } from "./components/JustwonLogo";
+
+export { StudioHorizontalLogo } from "./components/StudioHorizontalLogo";
+export { StudioLogo } from "./components/StudioLogo";
