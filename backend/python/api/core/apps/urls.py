4917d79ffbbda1e1e668c6c187ed187dc55074d9
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-03-09 13:47:35 +0900

    add me API to router

--- backend/python/api/core/apps/urls.py
+++ backend/python/api/core/apps/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
+from .auth.user.views import MeAPIView
 from .auth.views import LoginView, LogoutView, TokenRefreshView
 
 router = DefaultRouter()
 
 urlpatterns = [
+  path("me/", MeAPIView.as_view(), name="me"),
   path("login/", LoginView.as_view(), name="login"),
   path("logout/", LogoutView.as_view(), name="logout"),
   path("tokens/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
