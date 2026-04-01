278721577a20eb7028d68339a1835beeb230f0f5
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-04-01 13:59:44 +0900

    simplify eslint configs

--- frontend/apps/studio/eslint.config.ts
+++ frontend/apps/studio/eslint.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "eslint/config";
-import pluginQuery from "@tanstack/eslint-plugin-query";
-import pluginRouter from "@tanstack/eslint-plugin-router";
 import { viteConfig } from "@justwon/eslint-config/vite";
 
 export default defineConfig([
-  ...pluginQuery.configs["flat/recommended"],
-  ...pluginRouter.configs["flat/recommended"],
   {
     extends: [viteConfig],
     languageOptions: {
