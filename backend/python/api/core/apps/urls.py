28d0992e758b6ada96f5b2280af9d462dce3c7f8
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-04-01 13:08:30 +0900

    route apis

--- backend/python/api/core/apps/urls.py
+++ backend/python/api/core/apps/urls.py
@@ -3,8 +3,13 @@ from rest_framework.routers import DefaultRouter
 
 from .auth.user.views import MeAPIView
 from .auth.views import LoginView, LogoutView, TokenRefreshView
+from .blog.categories.views import CategoryViewSet
+from .blog.posttypes.views import PostTypeViewSet
 
 router = DefaultRouter()
+
+router.register(r"blog/categories", CategoryViewSet, basename="category")
+router.register(r"blog/posttypes", PostTypeViewSet, basename="posttype")
 
 urlpatterns = [
   path("me/", MeAPIView.as_view(), name="me"),
